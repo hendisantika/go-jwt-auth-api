216e66929a98ac206bb46601e5b60f6af759da65
hendisantika <hendisantika@yahoo.co.id>
2024-08-10 19:03:28 +0700

    Add routes.go 🫸🌀✏️📗🐧🐳

---
+++ routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/hendisantika/go-jwt-auth-api/controllers"
+)
+
+func Setup(app *fiber.App) {
+	api := app.Group("/user")
+
+	api.Get("/get-user", controllers.User)
+
+	api.Post("/register", controllers.Register)
+
+	api.Post("/login", controllers.Login)
+
+	api.Post("/logout", controllers.Logout)
+}

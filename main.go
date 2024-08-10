63f7f1d545d42bc7aa70c9234a07ea6edb82a005
hendisantika <hendisantika@yahoo.co.id>
2024-08-10 19:46:01 +0700

    Bug Fix code :three: 🫸🌀✏️📗🐧🐳

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"go-jwt-auth-api/routes"
 )
@@ -12,16 +11,6 @@ import (
 func main() {
 	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
 	// to see how GoLand suggests fixing it.
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session,
-		// right-click your code in the editor and select the <b>Debug</b> option.
-		fmt.Println("i =", 100/i)
-	}
-
 	app := fiber.New()
 
 	routes.Setup(app) // A routes package/folder is created with 'Setup' function created.

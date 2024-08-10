1637622fbdcc1ec101ce2d4b871cb0a6b5c479b6
hendisantika <hendisantika@yahoo.co.id>
2024-08-10 19:07:37 +0700

    Add User in controllers.go 🫸🌀✏️📗🐧🐳

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -84,3 +84,27 @@ func Login(c *fiber.Ctx) error {
 		"message": "success",
 	})
 }
+
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	var user models.User
+
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+
+	return c.JSON(user)
+
+}

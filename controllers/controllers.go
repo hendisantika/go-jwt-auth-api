50e6cab4de0ccc479ea4e3532becbbf9ca2e1990
hendisantika <hendisantika@yahoo.co.id>
2024-08-10 19:07:50 +0700

    Add Logout in controllers.go 🫸🌀✏️📗🐧🐳

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -108,3 +108,19 @@ func User(c *fiber.Ctx) error {
 	return c.JSON(user)
 
 }
+
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour), //Sets the expiry time an hour ago in the past.
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+
+}
